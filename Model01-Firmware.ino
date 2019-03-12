1c94c61cd44ab4192b9032a04d97d6ffb0f9bb87
Lisa Ugray <lisa.ugray@shopify.com>
2019-03-12 06:53:30 -0400

    Fix volume controls

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -69,12 +69,12 @@ KEYMAPS(
     ___,          Key_Delete,      ___,         ___,        /**/           /**/              /**/
     ___,          /**/             /**/         /**/        /**/           /**/              /**/
 
-    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-    /**/                        Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-    ShiftToLayer(BUTTERFLY_FN), Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-    ___,                        ___,                    Key_Enter,                ___,                      /**/             /**/              /**/
-    ___                         /**/                    /**/                      /**/                      /**/             /**/              /**/
+    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,               Key_F8,                   Key_F9,                   Key_F10,          Key_F11,
+    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket, Key_RightCurlyBracket,    Key_LeftBracket,          Key_RightBracket, Key_F12,
+    /**/                        Key_LeftArrow,          Key_DownArrow,        Key_UpArrow,              Key_RightArrow,           ___,              ___,
+    ShiftToLayer(BUTTERFLY_FN), ___,                    Consumer_Mute,        Consumer_VolumeDecrement, Consumer_VolumeIncrement, Key_Backslash,    Key_Pipe,
+    ___,                        ___,                    Key_Enter,            ___,                      /**/                      /**/              /**/
+    ___                         /**/                    /**/                  /**/                      /**/                      /**/              /**/
 
   ), [BUTTERFLY] = KEYMAP_STACKED(
 
