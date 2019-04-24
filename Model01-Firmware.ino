95bd6b9c23db6f6412d03642d397f63354ec286f
Lisa Ugray <lisa.ugray@shopify.com>
2019-04-24 08:55:11 -0400

    Underscore key

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -35,12 +35,12 @@ KEYMAPS(
     /**/          /**/   /**/   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
     /**/          /**/   /**/   /**/             /**/           /**/         ShiftToLayer(L_FN),
 
-    Key_Sleep,               Key_6,        Key_7,        Key_8,            Key_9,      Key_0,            LockLayer(NUMPAD),
-    Key_Enter,               Key_Y,        Key_U,        Key_I,            Key_O,      Key_P,            Key_Equals,
-    /**/                     Key_H,        Key_J,        Key_K,            Key_L,      TOPSY(Semicolon), Key_Quote,
-    ShiftToLayer(BUTTERFLY), Key_N,        Key_M,        Key_Comma,        Key_Period, Key_Slash,        Key_Minus,
-    Key_RightShift,          Key_RightGui, Key_Spacebar, Key_RightControl, /**/        /**/              /**/
-    ShiftToLayer(L_FN)       /**/          /**/          /**/              /**/        /**/              /**/
+    Key_Sleep,         Key_6,        Key_7,        Key_8,            Key_9,      Key_0,            LockLayer(NUMPAD),
+    Key_Enter,         Key_Y,        Key_U,        Key_I,            Key_O,      Key_P,            Key_Equals,
+    /**/               Key_H,        Key_J,        Key_K,            Key_L,      TOPSY(Semicolon), Key_Quote,
+    TOPSY(Minus),      Key_N,        Key_M,        Key_Comma,        Key_Period, Key_Slash,        Key_Minus,
+    Key_RightShift,    Key_RightGui, Key_Spacebar, Key_RightControl, /**/        /**/              /**/
+    ShiftToLayer(L_FN) /**/          /**/          /**/              /**/        /**/              /**/
 
   ), [NUMPAD] = KEYMAP_STACKED(
 
