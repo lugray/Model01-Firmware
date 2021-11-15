354a19ff0087dca05e6d491706e6fb4ecbfe6084
Lisa Ugray <lisa.ugray@shopify.com>
2021-11-15 10:41:07 -0500

    I use - more than I realized

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -49,10 +49,10 @@ KEYMAPS(
     /**/          /**/   /**/   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
     /**/          /**/   /**/   /**/             /**/           /**/         STL(FN),
 
-    Sleep,          Key_6,        Key_7,        Key_8,            Key_9,      Key_0,            STL(REACT),
+    Sleep,          Key_6,        Key_7,        Key_8,            Key_9,      Key_0,            Key_Backslash,
     Key_Enter,      Key_Y,        Key_U,        Key_I,            Key_O,      Key_P,            Key_Equals,
     /**/            Key_H,        Key_J,        Key_K,            Key_L,      TOPSY(Semicolon), Key_Quote,
-    TOPSY(Minus),   Key_N,        Key_M,        Key_Comma,        Key_Period, Key_Slash,        Key_Backslash,
+    TOPSY(Minus),   Key_N,        Key_M,        Key_Comma,        Key_Period, Key_Slash,        Key_Minus,
     Key_RightShift, Key_RightGui, Key_Spacebar, Key_RightControl, /**/        /**/              /**/
     STL(FN)         /**/          /**/          /**/              /**/        /**/              /**/
 
@@ -68,7 +68,7 @@ KEYMAPS(
     DM(DM_ANY), Key_F6,        Key_F7,        Key_F8,      Key_F9,         Key_F10,       Key_F11,
     WinMax,     ___,           ___,           ___,         ___,            ___,           Key_F12,
     /**/        Key_LeftArrow, Key_DownArrow, Key_UpArrow, Key_RightArrow, Key_Semicolon, TOPSY(Quote),
-    Key_Minus,  ___,           Mute,          VolDown,     VolUp,          TOPSY(Slash),  TOPSY(Backslash),
+    Key_Minus,  ___,           Mute,          VolDown,     VolUp,          TOPSY(Slash),  Key_Backslash,
     ___,        ___,           Key_Enter,     ___,         /**/            /**/           /**/
     STL(EMOJI), /**/           /**/           /**/         /**/            /**/           /**/
 
