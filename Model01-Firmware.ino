ef33ab27da9d394a64d2e785c4f47cb7f98e794e
Lisa Ugray <lisa.ugray@shopify.com>
2021-11-15 10:47:40 -0500

    More definitions

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -38,23 +38,33 @@ enum { L_PRIMARY, L_FN, L_EMOJI, L_REACT, L_QUINN, L_QFN }; // layers
 #define Mute Consumer_Mute
 #define VolDown Consumer_VolumeDecrement
 #define VolUp Consumer_VolumeIncrement
+#define LEDNext Key_LEDEffectNext
+#define LAlt Key_LeftAlt
+#define LCtrl Key_LeftControl
+#define RCtrl Key_RightControl
+#define LCmd Key_LeftGui
+#define RCmd Key_RightGui
+#define LShift Key_LeftShift
+#define RShift Key_RightShift
+#define BkSpc Key_Backspace
+#define SpcBar Key_Spacebar
 
 KEYMAPS(
   [L_PRIMARY] = KEYMAP_STACKED(
 
-    STL(REACT),   Key_1, Key_2, Key_3,           Key_4,         Key_5,       Key_LEDEffectNext,
-    Key_Backtick, Key_Q, Key_W, Key_E,           Key_R,         Key_T,       Key_Tab,
-    Key_Home,     Key_A, Key_S, Key_D,           Key_F,         Key_G,       /**/
-    Key_End,      Key_Z, Key_X, Key_C,           Key_V,         Key_B,       Key_LeftAlt,
-    /**/          /**/   /**/   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-    /**/          /**/   /**/   /**/             /**/           /**/         STL(FN),
+    STL(REACT),   Key_1, Key_2, Key_3, Key_4, Key_5, LEDNext,
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+    Key_Home,     Key_A, Key_S, Key_D, Key_F, Key_G, /**/
+    Key_End,      Key_Z, Key_X, Key_C, Key_V, Key_B, LAlt,
+    /**/          /**/   /**/   LCtrl, BkSpc, LCmd,  LShift,
+    /**/          /**/   /**/   /**/   /**/   /**/   STL(FN),
 
-    Sleep,          Key_6,        Key_7,        Key_8,            Key_9,      Key_0,            Key_Backslash,
-    Key_Enter,      Key_Y,        Key_U,        Key_I,            Key_O,      Key_P,            Key_Equals,
-    /**/            Key_H,        Key_J,        Key_K,            Key_L,      TOPSY(Semicolon), Key_Quote,
-    TOPSY(Minus),   Key_N,        Key_M,        Key_Comma,        Key_Period, Key_Slash,        Key_Minus,
-    Key_RightShift, Key_RightGui, Key_Spacebar, Key_RightControl, /**/        /**/              /**/
-    STL(FN)         /**/          /**/          /**/              /**/        /**/              /**/
+    Sleep,        Key_6, Key_7,  Key_8,     Key_9,      Key_0,            Key_Backslash,
+    Key_Enter,    Key_Y, Key_U,  Key_I,     Key_O,      Key_P,            Key_Equals,
+    /**/          Key_H, Key_J,  Key_K,     Key_L,      TOPSY(Semicolon), Key_Quote,
+    TOPSY(Minus), Key_N, Key_M,  Key_Comma, Key_Period, Key_Slash,        Key_Minus,
+    RShift,       RCmd,  SpcBar, RCtrl,     /**/        /**/              /**/
+    STL(FN)       /**/   /**/    /**/       /**/        /**/              /**/
 
   ), [L_FN] = KEYMAP_STACKED (
 
