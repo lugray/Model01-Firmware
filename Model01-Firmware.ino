37750150e84cabceb5cc6300179bb1cce5bb62d8
Lisa Ugray <lisa.ugray@shopify.com>
2021-11-15 10:49:11 -0500

    Unstack all keymaps

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -50,37 +50,23 @@ enum { L_PRIMARY, L_FN, L_EMOJI, L_REACT, L_QUINN, L_QFN }; // layers
 #define SpcBar Key_Spacebar
 
 KEYMAPS(
-  [L_PRIMARY] = KEYMAP_STACKED(
+  [L_PRIMARY] = KEYMAP(
 
-    STL(REACT),   Key_1, Key_2, Key_3, Key_4, Key_5, LEDNext,
-    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-    Key_Home,     Key_A, Key_S, Key_D, Key_F, Key_G, /**/
-    Key_End,      Key_Z, Key_X, Key_C, Key_V, Key_B, LAlt,
-    /**/          /**/   /**/   LCtrl, BkSpc, LCmd,  LShift,
-    /**/          /**/   /**/   /**/   /**/   /**/   STL(FN),
+    STL(REACT),   Key_1, Key_2, Key_3, Key_4, Key_5, LEDNext, Sleep,        Key_6, Key_7,  Key_8,     Key_9,      Key_0,            Key_Backslash,
+    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab, Key_Enter,    Key_Y, Key_U,  Key_I,     Key_O,      Key_P,            Key_Equals,
+    Key_Home,     Key_A, Key_S, Key_D, Key_F, Key_G, /**/     /**/          Key_H, Key_J,  Key_K,     Key_L,      TOPSY(Semicolon), Key_Quote,
+    Key_End,      Key_Z, Key_X, Key_C, Key_V, Key_B, LAlt,    TOPSY(Minus), Key_N, Key_M,  Key_Comma, Key_Period, Key_Slash,        Key_Minus,
+    /**/          /**/   /**/   LCtrl, BkSpc, LCmd,  LShift,  RShift,       RCmd,  SpcBar, RCtrl,     /**/        /**/              /**/
+    /**/          /**/   /**/   /**/   /**/   /**/   STL(FN), STL(FN)       /**/   /**/    /**/       /**/        /**/              /**/
 
-    Sleep,        Key_6, Key_7,  Key_8,     Key_9,      Key_0,            Key_Backslash,
-    Key_Enter,    Key_Y, Key_U,  Key_I,     Key_O,      Key_P,            Key_Equals,
-    /**/          Key_H, Key_J,  Key_K,     Key_L,      TOPSY(Semicolon), Key_Quote,
-    TOPSY(Minus), Key_N, Key_M,  Key_Comma, Key_Period, Key_Slash,        Key_Minus,
-    RShift,       RCmd,  SpcBar, RCtrl,     /**/        /**/              /**/
-    STL(FN)       /**/   /**/    /**/       /**/        /**/              /**/
+  ), [L_FN] = KEYMAP(
 
-  ), [L_FN] = KEYMAP_STACKED (
-
-    ___,          Key_F1,  Key_F2,  Key_F3,   Key_F4,     Key_F5, M(TOGGLE_QUINN),
-    ___,          ___,     WinUp,   ___,      ___,        ___,    ___,
-    Key_PageUp,   WinLeft, WinDown, WinRight, ___,        ___,    /**/
-    Key_PageDown, ___,     ___,     ___,      ___,        ___,    ___,
-    /**/          /**/     /**/     ___,      Key_Delete, ___,    ___,
-    /**/          /**/     /**/     /**/      /**/        /**/    STL(EMOJI),
-
-    DM(DM_ANY), Key_F6,        Key_F7,        Key_F8,      Key_F9,         Key_F10,       Key_F11,
-    WinMax,     ___,           ___,           ___,         ___,            ___,           Key_F12,
-    /**/        Key_LeftArrow, Key_DownArrow, Key_UpArrow, Key_RightArrow, Key_Semicolon, TOPSY(Quote),
-    Key_Minus,  ___,           Mute,          VolDown,     VolUp,          TOPSY(Slash),  Key_Backslash,
-    ___,        ___,           Key_Enter,     ___,         /**/            /**/           /**/
-    STL(EMOJI), /**/           /**/           /**/         /**/            /**/           /**/
+    ___,          Key_F1,  Key_F2,  Key_F3,   Key_F4,     Key_F5, M(TOGGLE_QUINN), DM(DM_ANY), Key_F6,        Key_F7,        Key_F8,      Key_F9,         Key_F10,       Key_F11,
+    ___,          ___,     WinUp,   ___,      ___,        ___,    ___,             WinMax,     ___,           ___,           ___,         ___,            ___,           Key_F12,
+    Key_PageUp,   WinLeft, WinDown, WinRight, ___,        ___,    /**/             /**/        Key_LeftArrow, Key_DownArrow, Key_UpArrow, Key_RightArrow, Key_Semicolon, TOPSY(Quote),
+    Key_PageDown, ___,     ___,     ___,      ___,        ___,    ___,             Key_Minus,  ___,           Mute,          VolDown,     VolUp,          TOPSY(Slash),  Key_Backslash,
+    /**/          /**/     /**/     ___,      Key_Delete, ___,    ___,             ___,        ___,           Key_Enter,     ___,         /**/            /**/           /**/
+    /**/          /**/     /**/     /**/      /**/        /**/    STL(EMOJI),      STL(EMOJI), /**/           /**/           /**/         /**/            /**/           /**/
 
   ), [L_EMOJI] = KEYMAP(
 
